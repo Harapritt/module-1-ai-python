65ce8bc2e349408ae5ce4ec9d748749dff3e2d41
Harapritt <hd1122870@gmail.com>
2026-08-10 07:07:02 +0530

    basic_programs.py
    Basic Python programs

---
+++ day-2/basic_programs.py
@@ -0,0 +1,46 @@
+# Day 2: Basic Python Programs
+
+
+# 1. Add two numbers
+a = 10
+b = 20
+
+sum_result = a + b
+
+print("Sum:", sum_result)
+
+
+# 2. Calculate area of a rectangle
+length = 10
+width = 5
+
+area = length * width
+
+print("Area of rectangle:", area)
+
+
+# 3. Convert Celsius to Fahrenheit
+celsius = 25
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
+
+
+# 4. Check whether a number is even or odd
+number = 15
+
+if number % 2 == 0:
+    print(number, "is even")
+else:
+    print(number, "is odd")
+
+
+# 5. Find the largest of two numbers
+x = 50
+y = 30
+
+if x > y:
+    print("Largest number:", x)
+else:
+    print("Largest number:", y)
